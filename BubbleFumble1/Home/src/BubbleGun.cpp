02bb106825e2ad0d3066d95c6b9cb06005d0ed17
6KGFZ <141700790+6KGFZ@users.noreply.github.com>
2025-12-19 17:26:26 +0800

    Delete BubbleFumble1/Home/src directory

--- BubbleFumble1/Home/src/BubbleGun.cpp
+++
@@ -1,9 +0,0 @@
-// BubbleGun.cpp
-#include "BubbleGun.hpp"
-#include <graphics.h>
-
-void BubbleGun::show() {
-    setcolor(DARKGRAY);
-    for (int i = 0; i < 5; ++i)
-        rectangle(100 + i * 120, 550, 130 + i * 120, 580);
-}
